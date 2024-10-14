b62c401e1ccce30c3461c2e7dd3dee013fe3d706
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2024-10-14 00:42:32 +0000

    fix: use bash script for compiling plugins

---
+++ scripts/compile_plugins.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Get the current working directory and resolve its absolute path
+rootPath=$(pwd)
+
+# Find all .go files in the 'pl' directory
+files=$(find "$rootPath/pl" -name "*.go")
+
+# Loop through each .go file found
+for file in $files; do
+  # Extract the filename without the extension
+  fileName=$(basename "$file")
+  fileNameWithoutExt="${fileName%.go}"
+
+  # Define the input and output files
+  outputFile="pl/${fileNameWithoutExt}.so"
+  inputFile="pl/${fileNameWithoutExt}.go"
+
+  # Build the Go plugin
+  go build -buildmode=plugin -o "$outputFile" "$inputFile"
+
+  # Check if the build was successful
+  if [ $? -eq 0 ]; then
+    echo "Built module: $fileNameWithoutExt"
+  else
+    echo "Failed to build module: $fileNameWithoutExt"
+    exit 1
+  fi
+done
